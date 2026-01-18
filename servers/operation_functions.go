88d1b370079f3815cdab07a6fd2c2ab1d9d2713a
Jacobson-H <Jacobson-H@users.noreply.github.com>
2026-01-18 01:50:01 +0000

    Added file hash function to allow the SHA256 hash of a file to be used within a operation condition

--- servers/operation_functions.go
+++ servers/operation_functions.go
@@ -1,12 +1,16 @@
 package servers
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
 	"github.com/google/cel-go/interpreter/functions"
 	"github.com/pufferpanel/pufferpanel/v3"
+	"github.com/pufferpanel/pufferpanel/v3/logging"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -26,11 +30,18 @@ func CreateFunctions(env *pufferpanel.Environment) []cel.EnvOption {
 				cel.BoolType,
 				cel.UnaryBinding(cel_in_path(env)),
 			)),
-		cel.Function("is_server_running", cel.Overload("is_server_running_bool",
-			[]*cel.Type{},
-			cel.BoolType,
-			cel.FunctionBinding(cel_is_server_running(env)),
-		)),
+		cel.Function("is_server_running",
+			cel.Overload("is_server_running_bool",
+				[]*cel.Type{},
+				cel.BoolType,
+				cel.FunctionBinding(cel_is_server_running(env)),
+			)),
+		cel.Function("file_hash",
+			cel.Overload("file_hash_string_string",
+				[]*cel.Type{cel.StringType},
+				cel.StringType,
+				cel.UnaryBinding(cel_file_hash(env)),
+			)),
 	}
 }
 
@@ -53,5 +64,36 @@ func cel_is_server_running(env *pufferpanel.Environment) functions.FunctionOp {
 	return func(values ...ref.Val) ref.Val {
 		r, err := env.IsRunning()
 		return types.Bool(err == nil && r)
+	}
+}
+
+func cel_file_hash(env *pufferpanel.Environment) functions.UnaryOp {
+	return func(fileName ref.Val) ref.Val {
+		fullPath := filepath.Join(env.GetRootDirectory(), fileName.Value().(string))
+		_, err := os.Stat(fullPath)
+
+		if err != nil {
+			if os.IsNotExist(err) {
+				logging.Error.Printf("Target file does not exist")
+				return nil
+			} else if !os.IsNotExist(err) {
+				logging.Error.Printf("Error reading target file: %s", err)
+				return nil
+			}
+		}
+
+		file, err := os.Open(fullPath)
+		if err != nil {
+			logging.Error.Printf("Error opening target file: %s", err)
+			return nil
+		}
+		defer file.Close()
+
+		hasher := sha256.New()
+		if _, err := io.Copy(hasher, file); err != nil {
+			return nil
+		}
+		fileHash := hex.EncodeToString(hasher.Sum(nil))
+		return types.String(fileHash)
 	}
 }
